c9324446186902c17d8f585671a9bed02064529f
SEBASTIAN-ALES1 <>
2022-04-23 22:36:19 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -46,33 +46,35 @@ TreeMap * createTreeMap(int (*lower_than) (void* key1, void* key2)) {
 
 
 void insertTreeMap(TreeMap * tree, void* key, void * value) {
-  if (tree == NULL) return;
-
-  TreeNode *aux =tree->root;
-  TreeNode *parent =NULL;
-
-  while (aux!=NULL){
-  if (tree->lower_than(key,aux->pair->key)){
-    aux=aux->left;
-  }
-  if (tree->lower_than(aux->pair->key,key)){
-    aux=aux->right;
-  }else return;
-  }
-
-  
-  TreeNode *nuevo =createTreeNode(key,value);
-  nuevo->parent = parent;
-
-  if (parent==NULL){
-    tree->root= nuevo;
-  }
-  if (tree->lower_than(key,parent->pair->key)){
-    parent->left =nuevo;
-  } else parent->right = nuevo;
-
-  tree->current =nuevo;
-  
+    if (tree == NULL) return;
+    
+    TreeNode * aux = tree->root;
+    TreeNode * parent = NULL;
+    
+    while (aux != NULL) {
+        parent = aux;
+        
+        if (tree->lower_than(key,aux->pair->key)) {
+            aux = aux->left;
+        } else if (tree->lower_than(aux->pair->key,key)) {
+            aux = aux->right;
+        } else {
+            return;
+        }
+    }
+    
+    TreeNode * new = createTreeNode(key, value);
+    
+    new->parent = parent;
+    
+    if (parent == NULL) {
+        tree->root = new;
+    } else if (tree->lower_than(key,parent->pair->key)) {
+        parent->left = new;
+    } else {
+        parent->right = new;
+    }
+    tree->current = new;
 }
 
 TreeNode * minimum(TreeNode * x){
